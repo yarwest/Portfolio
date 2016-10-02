2283299f75de4020166b26e47be23e8f05c92725
Yarno Boelens <yarnoboelens@gmail.com>
2016-10-02 14:16:13 +0200

    Minor: various small stuff

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -14,25 +14,25 @@
                 <div class="form-group">
                     <label for="nom" class="col-sm-4 control-label">Full Name</label>
                     <div class="col-sm-8">
-                        {{ Form::text('name',null,['class'=>'form-control','id'=>'name','placeholder'=>'Ex: Barry White'])}}
+                        {{ Form::text('name',null,['class'=>'form-control','id'=>'name','placeholder'=>'Ex: Barry White', 'required'])}}
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="email" class="col-sm-4 control-label">Email Address</label>
                     <div class="col-sm-8">
-                        {{ Form::email('email',null,['class'=>'form-control','id'=>'email','placeholder'=>'Ex: barry@gmail.com']) }}
+                        {{ Form::email('email',null,['class'=>'form-control','id'=>'email','placeholder'=>'Ex: barry@gmail.com', 'required']) }}
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="phone" class="col-sm-4 control-label">Phone Number</label>
                     <div class="col-sm-8">
-                        {{ Form::text('phone',null,['class'=>'form-control','id'=>'phone','placeholder'=>'Ex: 0685164893']) }}
+                        {{ Form::text('phone',null,['class'=>'form-control','id'=>'phone','placeholder'=>'Ex: 0685164893', 'required']) }}
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="phone" class="col-sm-4 control-label">Message</label>
                     <div class="col-sm-8">
-                        {{ Form::textarea('phone',null,['class'=>'form-control','id'=>'phone','placeholder'=>'Enter your message here']) }}
+                        {{ Form::textarea('phone',null,['class'=>'form-control','id'=>'phone','placeholder'=>'Enter your message here', 'required']) }}
                     </div>
                 </div>
                 <div class="text-right">
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,6 +3,9 @@
         <ul>
             <li><a href="/contact">Contact</a></li>
         </ul>
+        <p style="float: left; margin-left: 150px;">
+            Designed by: <a href="http://oussamabennaci.com/">Oussama Bennaci</a>
+        </p>
         <p class="text-center">
             &copy; 2016, Yarwest.
         </p>
