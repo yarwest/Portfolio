6dfb9ae2009403ee255d96fe6d875873a9837e6a
yarwest <yarnoboelens@gmail.com>
2018-05-15 19:51:05 +0200

    Removed deadlink from footer.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,7 +6,7 @@
             <li><a href="/contact">Contact</a></li>
         </ul>
         <p class="text-center">
-            Designed by: Yarno Boelens and <a href="http://oussamabennaci.com/">Oussama Bennaci</a>
+            Designed by: Yarno Boelens
         </p>
     </div>
 </footer>
